e71d4d4dcd6196de1514e56bdb9d5313884bfbba
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-05-11 20:46:53 +0300

    лаба

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
+import random
 class Animal:
-    def __init__(self, name, age):
+    def __int__(self, species, name, age, health, hunger, happiness):
+        super().__init__()
+        self.species = species
         self.name = name
         self.age = age
+        self.healsh = health
+        self.hunger = hunger
+        self.happiness = happiness
+    def grow(self):
+        self.age += 1
+        self.healsh = random.randint(0, 10)
+        self.hunger = random.randint(0, 10)
+        self.happiness =  random.randint(0, 10)
     def eat(self):
-        print(self.name, 'нямкає')
+        if self.hunger >= 6:
+            self.healsh += random.randint(0, 5)
+            self.happiness += random.randint(0, 5)
+            self.healsh -= random.randint(5, 7)
+        else:
+            print(self.name, 'не голодний')
+    def play(self):
+        pass #happiness
 
-class Dog(Animal):
-    def __int__(self, name, age, breed):
-        super().__init__(name, age)
-        self.breed = breed
-    def bark(self):
-        print(self.name, 'гавкає')
-hotdog = Dog('bobik', 3,'labrodor')
-print(hotdog.name, hotdog.breed, hotdog.age)
-hotdog.eat()
+
+class Zoo:
+    def __init__(self):
+        super().__init__()
+        self.animal = []
